79f69c073a3d33de5c5af289420621f8f17bea37
Ivan Limongan <ivan.limongan@gmail.com>
2017-08-31 20:23:37 -0700

    randomizer works

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
 console.log("start");
 
+var imageNames = ["bag.jpg", "banana.jpg", "boots.jpg", "chair.jpg", "cthulhu.jpg", "dragon.jpg", "pen.jpg", "scissors.jpg", "shark.jpg", "sweep.jpg", "unicorn.jpg", "usb.jpg", "water_can.jpg", "wine_glass.jpg"];
+
+function addImg(imageFileName) {
+  var container = document.getElementById("imageContain");
+  var image = document.createElement("img");
+  image.src = imageFileName;
+  container.appendChild(image);
+}
 
 
+// function getRandom() {
+//   var index = Math.floor(Math.random() * 14);
+// }
+
+function showImages() {
+  var index = Math.floor(Math.random() * 14)
+  addImg("img/"+imageNames[index]);
+
+  index = Math.floor(Math.random() * 14)
+  addImg("img/"+imageNames[index]);
+
+  index = Math.floor(Math.random() * 14)
+  addImg("img/"+imageNames[index]);
+}
 
 
+window.addEventListener("load", showImages);
 console.log("end");
