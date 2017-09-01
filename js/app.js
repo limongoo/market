a804fa736740995df61e20da6b5ab894956306ec
Ivan Limongan <ivan.limongan@gmail.com>
2017-08-31 20:55:27 -0700

    random image

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ console.log("start");
 
 var imageNames = ["bag.jpg", "banana.jpg", "boots.jpg", "chair.jpg", "cthulhu.jpg", "dragon.jpg", "pen.jpg", "scissors.jpg", "shark.jpg", "sweep.jpg", "unicorn.jpg", "usb.jpg", "water_can.jpg", "wine_glass.jpg"];
 
+// function to add image
 function addImg(imageFileName) {
   var container = document.getElementById("imageContain");
   var image = document.createElement("img");
@@ -9,21 +10,28 @@ function addImg(imageFileName) {
   container.appendChild(image);
 }
 
+// random function
+function getRandom() {
+  return Math.floor(Math.random() * 14);
+} // end function
 
-// function getRandom() {
-//   var index = Math.floor(Math.random() * 14);
-// }
-
+// function to randomize image
 function showImages() {
-  var index = Math.floor(Math.random() * 14)
-  addImg("img/"+imageNames[index]);
+  var index1 = getRandom();
+  addImg("img/"+imageNames[index1]);
 
-  index = Math.floor(Math.random() * 14)
-  addImg("img/"+imageNames[index]);
+  var index2 = getRandom();
+  addImg("img/"+imageNames[index2]);
+    while (index2 === index1) {
+      index2 = getRandom();
+    }
 
-  index = Math.floor(Math.random() * 14)
-  addImg("img/"+imageNames[index]);
-}
+  var index3 = getRandom();
+  addImg("img/"+imageNames[index3]);
+    while (index3 === index2 || index3 === index1) {
+      index3 = getRandom();
+    }
+} //end function
 
 
 window.addEventListener("load", showImages);
