e1fc4262926994c882bfc133c12f57b15f5c7747
Ivan Limongan <ivan.limongan@gmail.com>
2017-09-03 10:35:40 -0700

    random works and clears div

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ console.log("start");
 
 var imageNames = ["bag.jpg", "banana.jpg", "boots.jpg", "chair.jpg", "cthulhu.jpg", "dragon.jpg", "pen.jpg", "scissors.jpg", "shark.jpg", "sweep.jpg", "unicorn.jpg", "usb.jpg", "water_can.jpg", "wine_glass.jpg"];
 
+
+
 // function to add image
 function addImg(imageFileName) {
   var container = document.getElementById("imageContain");
@@ -11,6 +13,11 @@ function addImg(imageFileName) {
   container.appendChild(image);
 }
 
+// var index1 = document.createElement("img");
+// var index2 = document.createElement("img");
+// var index3 = document.createElement("img");
+
+
 // random function
 function getRandom() {
   return Math.floor(Math.random() * 14);
@@ -18,34 +25,47 @@ function getRandom() {
 
 // function to randomize image
 function showImages() {
+
+  var container = document.getElementById("imageContain");
+  container.innerHTML = "";
+
   var index1 = getRandom();
   addImg("img/"+imageNames[index1]);
 
   var index2 = getRandom();
+  while (index2 === index1) {
+    index2 = getRandom();
+  }
   addImg("img/"+imageNames[index2]);
-    while (index2 === index1) {
-      index2 = getRandom();
-    }
 
   var index3 = getRandom();
+  while (index3 === index2 || index3 === index1) {
+    index3 = getRandom();
+  }
   addImg("img/"+imageNames[index3]);
-    while (index3 === index2 || index3 === index1) {
-      index3 = getRandom();
-    }
 }
+
+// // Click event listener
+// index1.addEventListener('click', recordClick);
+// index2.addEventListener('click', recordClick);
+// index3.addEventListener('click', recordClick);
+
 
 // function to record click
 function recordClick(event) {
   var imageSource = event.target.src;
   console.log("Image Clicked: "+imageSource);
 
+  // var image = document.createElement("img");
+  //
   // var index1 = document.getElementsByTagName("img");
+  // console.log(index1);
   // var index2 = document.getElementsByTagName("img");
   // var index3 = document.getElementsByTagName("img");
-  //
-  // index1 = document.removeChild("img");
-  // index2 = document.removeChild("img");
-  // index3 = document.removeChild("img");
+
+  // index1.removeChild(image);
+  // index2.removeChild(image);
+  // index3.removeChild(image);
 
   showImages();
 }
