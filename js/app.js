c05371c796490f754672998c09fc56cdf522f092
Ivan Limongan <ivan.limongan@gmail.com>
2017-09-05 18:22:24 -0700

    add basic canvas chart

--- js/app.js
+++ js/app.js
@@ -102,6 +102,33 @@ function recordClick(event) {
   showImages();
 }
 
+// chart add
+window.onload = function () {
+	var chart = new CanvasJS.Chart("chartContainer", {
+		theme: "theme2",//theme1
+		title:{
+			text: "Number of Clicks by Photo"
+		},
+		animationEnabled: true,   // change to true
+		data: [
+		{
+			// Change type to "bar", "area", "spline", "pie",etc.
+			type: "column",
+			dataPoints: [
+				{ label: "apple",  y: 10  },
+				{ label: "orange", y: 15  },
+				{ label: "banana", y: 25  },
+				{ label: "mango",  y: 30  },
+				{ label: "grape",  y: 28  }
+			]
+		}
+		]
+	});
+	chart.render();
+}
+
+
+
 
 // window event listener
 window.addEventListener("load", showImages);
